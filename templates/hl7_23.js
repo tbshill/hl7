c6f77c2c643150441e7262b681eeb4b874844af2
IT <shill.braden@gmail.com>
2019-03-21 19:46:39 -0600

    Implemented OBR segment, no tests

--- templates/hl7_23.js
+++ templates/hl7_23.js
@@ -184,6 +184,51 @@ const templates = {
     this.military_nonavailability_code = components => datatypes.parse_ID(components[35]);
     this.newborn_baby_indicator = components => datatypes.parse_ID(components[36]);
     this.baby_detained_indicator = components => datatypes.parse_ID(components[37]);
+  },
+  obr_template: function obr_template() {
+    this.set_id = components => datatypes.parse_SI(components[0]);
+    this.placer_order_number = components => datatypes.parse_EI(components[1]);
+    this.filler_order_number = components => datatypes.parse_EI(components[2]);
+    this.universal_service_id = components => datatypes.parse_CE(components[3]);
+    this.priority = components => datatypes.parse_ID(components[4]);
+    this.requested_time = components => datatypes.parse_TS(components[5]);
+    this.observation_time = components => datatypes.parse_TS(components[6]);
+    this.observation_end_time = components => datatypes.parse_TS(components[7]);
+    this.collection_volume = components => datatypes.parse_CQ(components[8]);
+    this.collector_identifier = components => datatypes.parse_XCN(components[9]);
+    this.specimen_action_code = components => datatypes.parse_ID(components[10]);
+    this.danger_code = components => datatypes.parse_CE(components[11]);
+    this.relevant_clinical_info = components => datatypes.parse_ST(components[12]);
+    this.specimen_recieved_time = components => datatypes.parse_TS(components[13]);
+    this.specimen_source = components => datatypes.parse_CM(components[14]);
+    this.ordering_provider = components => datatypes.parse_XCN(components[15]);
+    this.order_callback_phone_number = components => datatypes.parse_XTN(components[16]);
+    this.placer_feild1 = components => datatypes.parse_ST(components[17]);
+    this.placer_feild2 = components => datatypes.parse_ST(components[18]);
+    this.filler_feild1 = components => datatypes.parse_ST(components[19]);
+    this.filler_feild2 = components => datatypes.parse_ST(components[20]);
+    this.results_change_time = components => datatypes.parse_TS(components[21]);
+    this.charge_to_practice = components => datatypes.parse_CM(components[22]);
+    this.diagnostic_service_section_id = components => datatypes.parse_ID(components[23]);
+    this.result_status = components => datatypes.parse_ID(components[24]);
+    this.parent_result = components => datatypes.parse_CM(components[25]);
+    this.quantity_timing = components => datatypes.parse_TQ(components[26]);
+    this.result_copies_top = components => datatypes.parse_XCN(components[27]);
+    this.parent = components => datatypes.parse_CM(components[28]);
+    this.transporation_mode = components => datatypes.parse_ID(components[29]);
+    this.reason_for_study = components => datatypes.parse_CE(components[30]);
+    this.principal_result_interpreter = components => datatypes.parse_CM(components[31]);
+    this.assistant_result_interpreter = components => datatypes.parse_CM(components[32]);
+    this.technician = components => datatypes.parse_CM(components[33]);
+    this.transcriptionist = components => datatypes.parse_CM(components[34]);
+    this.scheduled_time = components => datatypes.parse_TS(components[35]);
+    this.number_of_sample_containers = components => datatypes.parse_NM(components[36]);
+    this.transport_logistics_of_sample = components => datatypes.parse_CE(components[37]);
+    this.collectors_comments = components => datatypes.parse_CE(components[38]);
+    this.transport_arrangement_responsibility = components => datatypes.parse_CE(components[39]);
+    this.transport_arranged = components => datatypes.parse_ID(components[40]);
+    this.escort_required = components => datatypes.parse_ID(components[41]);
+    this.planned_patient_transport_comment = components => datatypes.parse_CE(components[42]);
   }
 };
 
